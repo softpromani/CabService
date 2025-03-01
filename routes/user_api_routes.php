c12df01ef718c54b9404192f38a9db94d3d2ae76
killerheman <sharmahcool5@gmail.com>
2025-03-01 16:47:41 +0530

    user login

--- routes/user_api_routes.php
+++ routes/user_api_routes.php
@@ -3,12 +3,13 @@
 use App\Http\Controllers\Api\User\AuthController;
 use App\Http\Controllers\Api\User\BookingController;
 use App\Http\Controllers\Api\User\CustomerTicketController;
+use App\Http\Controllers\Api\User\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('user')->as('user.')->group(function () {
     Route::post('login', [AuthController::class, 'userLogin'])->name('login');
     Route::middleware(['auth:api'])->group(function () {
-        Route::post('/update-profile', [AuthController::class, 'userProfileupdate'])->name('userProfileupdate');
+        Route::post('update-profile', [ProfileController::class, 'userProfileupdate'])->name('userProfileupdate');
 
         Route::group(['prefix' => 'support-ticket'], function () {
             Route::controller(CustomerTicketController::class)->group(function () {
