1cfec8547bdaec58159d648f8921a3bb66e055cc
killerheman <sharmahcool5@gmail.com>
2025-03-28 12:33:21 +0530

    segment

--- app/Models/RideSeatSegment.php
+++ app/Models/RideSeatSegment.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class RideSeatSegment extends Model
 {
+    protected $guarded = [];
     public static function checkAvailableSeats($rideId, $pickupStationId, $dropoffStationId)
     {
         // Get total seats in the ride
