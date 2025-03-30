c416c126e1c64541ed321a8f7c2573cf4b36fa30
killerheman <sharmahcool5@gmail.com>
2025-03-30 13:33:42 +0530

    rider

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    protected $fillable = ['booking_number', 'user_id', 'ride_id', 'pickup_station_id', 'dropoff_station_id', 'total_distance', 'fare_amount', 'seats', 'status', 'comment'];
+    protected $fillable = ['booking_number', 'user_id', 'ride_id', 'rider_id', 'pickup_station_id', 'dropoff_station_id', 'total_distance', 'fare_amount', 'seats', 'status', 'comment'];
     protected $with     = ['station_origin', 'station_destination'];
     protected static function boot()
     {
